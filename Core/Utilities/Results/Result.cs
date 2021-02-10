f680aa0c165afa9fd4e9d40401ce818782439588
koyuboy <35155252+koyuboy@users.noreply.github.com>
2021-02-11 00:16:00 +0300

    11.02.2021

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+        public Result(bool success, string message) : this(success) //bu sınıfın tek parametreli contructırınıda çalıştır
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        //getter lar readonly dir ve sadece constructer içinde set edilebilir
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
