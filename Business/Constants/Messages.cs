917df29e7ceccdcaabf19401f70fce83c38f3add
koyuboy <35155252+koyuboy@users.noreply.github.com>
2021-03-03 14:06:28 +0300

    JWT added 03.03.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -15,5 +16,15 @@ namespace Business.Constants
         public static string ProductUpdated = "Ürün güncellendi";
         public static string ProductNameAlreadyExistsError = "Aynı isimde bir ürün zaten kayıtlı";
         public static string CategoryLimitExceeded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+
+
     }
 }
--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -16,8 +17,11 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Product> Products { get; set; }  // hangi class hangi tablo sırayla bu demek
         public DbSet<Category> Categories { get; set; }  
         public DbSet<Customer> Customers { get; set; }  
-        public DbSet<Order> Orders { get; set; }  
-        
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
 
 
     }
